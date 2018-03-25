e9d9cc54edbd6ba1b55ab2f9f6391aa56761f71a
Layout example <x16137108@ncirl.ie>
2018-03-25 18:10:39 +0000

    started book scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180318173413) do
+ActiveRecord::Schema.define(version: 20180325172504) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "author"
+    t.string "subject"
+    t.string "publisher"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
