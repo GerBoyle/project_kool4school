c25d68a33fb282cc3cb0622e600fa6472bae5526
Layout example <x16137108@ncirl.ie>
2018-03-26 12:33:25 +0000

    added search view

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   root 'welcome#index'
-  get 'books-path', to: "books#index"
+  get 'books_path', to: "books#index"
+  get 'my_books', to:'users#my_books'
 end
