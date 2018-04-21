a6f0ad89c84a518d1f89984c7bc9a04fc479b47f
David Larkin <dlarkin365@gmail.com>
2018-04-21 21:07:00 +0000

    Update to Basket

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,20 @@ class UsersController<ApplicationController
     def my_profile
         @user = current_user
     end
-    
+
+=begin
+my_basket is configured to the current user 
+=end
+
     def my_basket
         @user = current_user
     end
+
+
+=begin
+add_to_basket includes an if statement. If the users basket already exists it adds the book details
+Else it will create the basket
+=end
 
     def add_to_basket
         if Basket.exists?(user_id: params[:user_id], book_id: params[:book_id])
@@ -22,6 +32,11 @@ class UsersController<ApplicationController
         end
         redirect_to my_basket_path(params[:user_id])
     end
+
+=begin
+Remove basket allows the user to clear down their basket by destroying it for the given user.
+it then redirects them to an empty basket 
+=end 
 
     def remove_from_basket
         @basket = Basket.find_by(user_id: params[:user_id], book_id: params[:book_id])
