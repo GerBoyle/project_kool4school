accb071ea570ab3ddb13d6622ffdad9581af1e4e
David Larkin <dlarkin365@gmail.com>
2018-04-21 20:44:01 +0000

    Update Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+=begin
+Below prevents users who are not logged in from the abilty to do these actions
+=end
+
   before_action :authenticate_user!, except: [ :index, :show, :search ]
 end
 
