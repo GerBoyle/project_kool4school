201a4d4076e4482b6b8221395ba648c8e959f9be
David Larkin <dlarkin365@gmail.com>
2018-04-22 16:57:17 +0000

    Update Protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+
+=begin
   protect_from_forgery with: :exception
+
+The above line was removed and the below line addded to prevent 
+GET HTTP requests as they are not secured with it.
+
+=end
+
+protect_from_forgery except :show 
+
 
 =begin
 Below prevents users who are not logged in from the abilty to do these actions
